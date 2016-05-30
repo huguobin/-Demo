ba1afa0ba3cdc4748cfee3c4bb457c2eea743522
hugb <1154684853@qq.com>
2016-05-30 14:43:38 +0800

    Initial Commit

---
+++ 桌面拖动分组Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  桌面拖动分组Demo
+//
+//  Created by huGb on 16/5/30.
+//  Copyright © 2016年 iyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 桌面拖动分组Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  桌面拖动分组Demo
+//
+//  Created by huGb on 16/5/30.
+//  Copyright © 2016年 iyd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
